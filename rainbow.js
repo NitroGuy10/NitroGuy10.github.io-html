5fef0e844e99c1e76fbcf322df10d0df8680458b
NitroGuy10 <nitroguy@tx.rr.com>
2016-12-15 12:15:34 -0600

    Update rainbow.js

--- rainbow.js
+++ rainbow.js
@@ -1,68 +1,43 @@
-
-red();
+var i = 0;
 function red() {
+if (i == 0) {
 setTimeout(function(){
-    var str = document.getElementById("bj").innerHTML;
+    var str = document.getElementById("rainbow").innerHTML;
     var result = str.fontcolor("red");
-    document.getElementById("bj").innerHTML = result;
-	color = "orange";
+    document.getElementById("rainbow").innerHTML = result;
+	i = 1;
      }, 1); 
 	 
 }
+}
 function orange() {
 setTimeout(function(){
-	var str = document.getElementById("bj").innerHTML;
+    var str = document.getElementById("rainbow").innerHTML;
     var result = str.fontcolor("orange");
-    document.getElementById("bj").innerHTML = result;
-}, 1000); 
+    document.getElementById("rainbow").innerHTML = result;
+     }, 1); 
+	 
 }
-/* function orange() {
-
-    setTimeout(function(){
-    var str = document.getElementById("bj").innerHTML;
-    var result = str.fontcolor("orange");
-    document.getElementById("bj").innerHTML = result;
-	yellow();
-    }, 1000); 
-    
-}
-function yellow() {
-
-    setTimeout(function(){
+/*function yellow() {
+setTimeout(function(){
     var str = document.getElementById("bj").innerHTML;
     var result = str.fontcolor("yellow");
     document.getElementById("bj").innerHTML = result;
-	green();
-    }, 1000); 
-  
-}
-function green() {
-
-    setTimeout(function(){
+     }, 1); 
+	 
+}function green() {
+setTimeout(function(){
     var str = document.getElementById("bj").innerHTML;
     var result = str.fontcolor("green");
     document.getElementById("bj").innerHTML = result;
-	blue();
-    }, 1000); 
-  
+     }, 1); 
+	 
 }
-function blue() {
-
-    setTimeout(function(){
+}function blue() {
+setTimeout(function(){
     var str = document.getElementById("bj").innerHTML;
     var result = str.fontcolor("blue");
     document.getElementById("bj").innerHTML = result;
-	purple();
-    }, 1000); 
-  
-}
-function purple() {
-
-    setTimeout(function(){
-    var str = document.getElementById("bj").innerHTML;
-    var result = str.fontcolor("purple");
-    document.getElementById("bj").innerHTML = result;
-	red();
-    }, 1000); 
-  
+     }, 1); 
+	 
 }*/
