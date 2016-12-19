ae69eb3d8ae57861489afc6d9f6ff4c323ba0c3c
NitroGuy10 <nitroguy@tx.rr.com>
2016-12-19 08:18:49 -0600

    Update rainbow.js

--- rainbow.js
+++ rainbow.js
@@ -1,65 +1,19 @@
-//iteration
-(function it() {
-setTimeout(function(){
-	var cycle;
-    document.getElementById("it").innerHTML = cycle.toString();
-    cycle = cycle + 1;
-     }, 1); 
-})();
-//version
-(function it() {
-setTimeout(function(){
-	document.getElementById("ver").innerHTML = "v3";
-     }, 1); 
-})();
-
-//script
-	    (function color() {
-setTimeout(function(){
-var cycle;
-    var color;
-	if (cycle > 5000) {color = "red"}
-    var str = document.getElementById("rainbow").innerHTML;
+var i = 0;
+var color = "black";
+setInterval(function(){
+i++; document.getElementById("rainbow").innerHTML = "Rainbow!";}, 300);
+var j = 1;
+setInterval(function(){
+if (i > 6) {
+i = 1;
+}
+if (i == 1) { color = "red"; }
+if (i == 2) { color = "orange"; }
+if (i == 3) { color = "#e0db59"; }
+if (i == 4) { color = "green"; }
+if (i == 5) { color = "blue"; }
+if (i == 6) { color = "purple"; }
+var str = document.getElementById("rainbow").innerHTML;
     var result = str.fontcolor(color);
     document.getElementById("rainbow").innerHTML = result;
-    cycle = cycle + 1;
-     }, 1); 
-})();
-/*function red() {
-setTimeout(function(){
-    var str = document.getElementById("rainbow").innerHTML;
-    var result = str.fontcolor("red");
-    document.getElementById("rainbow").innerHTML = result;
-     }, 1); 
-}}
-function orange() {
-setTimeout(function(){
-    var str = document.getElementById("rainbow").innerHTML;
-    var result = str.fontcolor("orange");
-    document.getElementById("rainbow").innerHTML = result;
-     }, 1); 
-	 
-}
-function yellow() {
-setTimeout(function(){
-    var str = document.getElementById("bj").innerHTML;
-    var result = str.fontcolor("yellow");
-    document.getElementById("bj").innerHTML = result;
-     }, 1); 
-	 
-}function green() {
-setTimeout(function(){
-    var str = document.getElementById("bj").innerHTML;
-    var result = str.fontcolor("green");
-    document.getElementById("bj").innerHTML = result;
-     }, 1); 
-	 
-}
-}function blue() {
-setTimeout(function(){
-    var str = document.getElementById("bj").innerHTML;
-    var result = str.fontcolor("blue");
-    document.getElementById("bj").innerHTML = result;
-     }, 1); 
-	 
-}*/
+}, 1);
